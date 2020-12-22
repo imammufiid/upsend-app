f6629a9bad828f18bd298a024f9f1840259d5364
imufiid <imufiid98@gmail.com>
2020-12-22 22:17:41 +0700

    api config

---
+++ app/src/main/java/com/mufiid/up_send/api/ApiServices.kt
@@ -0,0 +1,88 @@
+package com.mufiid.up_send.api
+
+import com.mufiid.up_send.data.EventEntity
+import com.mufiid.up_send.data.UserEntity
+import com.mufiid.up_send.data.source.remote.response.MessageResponse
+import com.mufiid.up_send.data.source.remote.response.WrappedListResponses
+import com.mufiid.up_send.data.source.remote.response.WrappedResponse
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.http.*
+
+interface ApiServices {
+    // LOGIN
+    @FormUrlEncoded
+    @POST("auth/login")
+    fun login(
+        @Field("email") email: String?,
+        @Field("password") password: String?
+    ): Observable<WrappedResponse<UserEntity>>
+
+    // REGISTRATION
+    @FormUrlEncoded
+    @POST("auth/register")
+    fun register(
+        @Field("username") username: String?,
+        @Field("firstname") firstName: String?,
+        @Field("lastname") lastName: String?,
+        @Field("email") email: String?,
+        @Field("password") password: String?
+    ): Observable<WrappedResponse<UserEntity>>
+
+    // GET EVENTS JOIN
+    @FormUrlEncoded
+    @POST("event/participant/events")
+    fun getListOfEventJoin(
+        @Header("Authorization") token: String?,
+        @Field("user_id") userId: String?
+    ): Observable<WrappedListResponses<EventEntity>>
+
+    // GET LIST PARTICIPANT JOIN
+    @FormUrlEncoded
+    @POST("event/participant")
+    fun getListOfParticipantJoin(
+        @Header("Authorization") token: String?,
+        @Field("event_id") eventId: String?
+    ): Observable<WrappedListResponses<UserEntity>>
+
+    // GET LIST PARTICIPANT JOIN
+    @FormUrlEncoded
+    @POST("event/participant/come")
+    fun getListOfParticipantCome(
+        @Header("Authorization") token: String?,
+        @Field("event_id") eventId: String?
+    ): Observable<WrappedListResponses<UserEntity>>
+
+    // GET EVENTS CREATED
+    @GET("event/")
+    fun getListOfEventCreated(
+        @Header("Authorization") token: String?,
+        @Query("user_id") userId: String?
+    ): Observable<WrappedListResponses<EventEntity>>
+
+    // GET SHOW EVENT
+    @GET("event/show")
+    fun getEventById(
+        @Header("Authorization") token: String?,
+        @Query("id") id: String?
+    ): Observable<WrappedListResponses<EventEntity>>
+
+    // GET SEARCH EVENT
+    @GET("event/search")
+    fun getEventBySearch(
+        @Header("Authorization") token: String?,
+        @Query("query") query: String?,
+        @Query("user_id") userId: String?
+    ): Observable<WrappedListResponses<EventEntity>>
+
+    // GET EVENTS CREATED
+    @DELETE("event/{id}")
+    fun deleteListOfEventCreated(
+        @Header("Authorization") token: String?,
+        @Path("id") id: String?
+    ): Observable<MessageResponse>
+
+    // CREATE EVENT
+
+    // UPDATE EVENT
+
+}
