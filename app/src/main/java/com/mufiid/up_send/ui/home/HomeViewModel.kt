8335bc9c5ca6dcb9f94cd66b7159e2a52bf96b7e
imufiid <imufiid98@gmail.com>
2020-12-24 12:33:15 +0700

    create home view model get event from API

---
+++ app/src/main/java/com/mufiid/up_send/ui/home/HomeViewModel.kt
@@ -0,0 +1,61 @@
+package com.mufiid.up_send.ui.home
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.mufiid.up_send.api.ApiConfig
+import com.mufiid.up_send.data.EventEntity
+import com.mufiid.up_send.utils.SingleLiveEvent
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class HomeViewModel:ViewModel() {
+    private var events = MutableLiveData<List<EventEntity>>()
+    private var state: SingleLiveEvent<EventState> = SingleLiveEvent()
+    private var api = ApiConfig.instance()
+
+    fun getAllDataByJoin(user_id: Int?, token: String?) {
+        state.value = EventState.IsLoading(true)
+        CompositeDisposable().add(
+            api.getListOfEventJoin("Bearer $token", user_id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    when(it.status) {
+                        200 -> events.postValue(it.data)
+                        else -> state.value = EventState.Error(it.message)
+                    }
+                    state.value = EventState.IsLoading()
+                }, {
+                    state.value = EventState.Error(it.message)
+                    state.value = EventState.IsLoading()
+                })
+        )
+    }
+
+    fun getEventsBySearch(token: String?, query: String?, userId: Int?) {
+        state.value = EventState.IsLoading(true)
+        CompositeDisposable().add(
+            api.getEventBySearch("Bearer $token", query, userId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    when(it.status) {
+                        200 -> events.postValue(it.data)
+                        else -> state.value = EventState.Error(it.message)
+                    }
+                    state.value = EventState.IsLoading()
+                }, {
+                    state.value = EventState.Error(it.message)
+                    state.value = EventState.IsLoading()
+                })
+        )
+    }
+
+    fun getEvents() = events
+    fun getState() = state
+}
+sealed class EventState() {
+    data class IsLoading(var state: Boolean = false): EventState()
+    data class Error(var err: String?): EventState()
+}
